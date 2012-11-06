464da18da8c973ba434872cdcd495b3abaa442a7
Amiram Kehaty <amiramke@gmail.com>
2012-11-05 18:51:47 -0800

    implement the migration change by creating a students table, created a Student class in app/models/student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+
+
+
+class Student < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :gender, :birthday
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Time.now.year - birthday.year
+  end
+
+end
